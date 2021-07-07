010f24f7626ed8668d141b15b4306a4dfb4be274
RubenMontes <rubenmontes330@gmail.com>
2021-07-07 17:59:03 +0100

    First Test

---
+++ src/main/java/com/MorseDecoder.java
@@ -0,0 +1,13 @@
+package com;
+
+public class MorseDecoder {
+
+
+    public Object decode(String s) {
+        if(s.equals(".-")){
+
+            return "A";
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/MorseDecoderTest.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MorseDecoderTest {
+    @Test
+    public void MorseDecoderTestA(){
+        MorseDecoder morseDecoder= new MorseDecoder();
+        assertEquals(morseDecoder.decode(".-"), "A");
+    }
+
+
+}
