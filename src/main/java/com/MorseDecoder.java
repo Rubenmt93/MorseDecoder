aeb798f0728b2a4e984566c23c435a119d13884b
RubenMontes <rubenmontes330@gmail.com>
2021-07-07 19:43:09 +0100

    Test Alphabeth

--- src/main/java/com/MorseDecoder.java
+++ src/main/java/com/MorseDecoder.java
@@ -11,12 +11,56 @@ public class MorseDecoder {
                 return "B";
             case "-.-.":
                 return "C";
+            case "-..":
+                return "D";
+            case ".":
+                return "E";
+            case "..-.":
+                return "F";
+            case"--.":
+                return "G";
+            case "....":
+                return "H";
+            case "..":
+                return "I";
+            case ".---":
+                return "J";
+            case "-.-":
+                return "K";
+            case ".-..":
+                return "L";
+            case "--":
+                return "M";
+            case "-.":
+                return "N";
+            case "---":
+                return "O";
+            case ".--.":
+                return "P";
+            case "--.-":
+                return "Q";
+            case ".-.":
+                return "R";
+            case "...":
+                return "S";
+            case "-":
+                return "T";
+            case "..-":
+                return "U";
+            case "...-":
+                return "V";
+            case".--":
+                return "W";
+            case "-..-" :
+                return "X";
+            case "-.--" :
+                return "Y";
+            case "--..":
+                return "Z";
             default:
                 return null;
         }
 
     }
 }
-
-
 
--- src/test/java/com/MorseDecoderTest.java
+++ src/test/java/com/MorseDecoderTest.java
@@ -18,5 +18,37 @@ public class MorseDecoderTest {
     public void MorseDecoderTestC(){
         assertEquals(morseDecoder.decode("-.-."),"C");
     }
+    @Test
+    public void getMorseDecoderTestAlphaBet() {
+        assertEquals(morseDecoder.decode(".-"),"A");
+        assertEquals(morseDecoder.decode("-..."),"B");
+        assertEquals(morseDecoder.decode("-.-."),"C");
+        assertEquals(morseDecoder.decode("-.."),"D");
+        assertEquals(morseDecoder.decode("."),"E");
+        assertEquals(morseDecoder.decode("..-."),"F");
+        assertEquals(morseDecoder.decode("--."),"G");
+        assertEquals(morseDecoder.decode("...."),"H");
+        assertEquals(morseDecoder.decode(".."),"I");
+        assertEquals(morseDecoder.decode("-.-"),"K");
+        assertEquals(morseDecoder.decode(".-.."),"L");
+        assertEquals(morseDecoder.decode("--"),"M");
+        assertEquals(morseDecoder.decode("-."), "N");
+        assertEquals(morseDecoder.decode("---"), "O");
+        assertEquals(morseDecoder.decode(".--."), "P");
+        assertEquals(morseDecoder.decode("--.-"), "Q");
+        assertEquals(morseDecoder.decode(".-."), "R");
+        assertEquals(morseDecoder.decode("..."), "S");
+        assertEquals(morseDecoder.decode("-"), "T");
+        assertEquals(morseDecoder.decode("..-"), "U");
+        assertEquals(morseDecoder.decode("...-"), "V");
+        assertEquals(morseDecoder.decode(".--"), "W");
+        assertEquals(morseDecoder.decode("-..-"), "X");
+        assertEquals(morseDecoder.decode("-.--"), "Y");
+        assertEquals(morseDecoder.decode("--.."), "Z");
+    }
 
+    @Test
+    public void getMorseDecoderTestFailed() {
+        assertEquals(morseDecoder.decode("#"), null);
+    }
 }
